0ef32b6273b1b811940fe3ba0c68bb3b112c1a72
Augusto Britos <augustobritos95@gmail.com>
2022-04-20 18:32:17 -0300

    My first commit on Git

---
+++ Java/Leccion 1/Hola Mundo/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,12 @@
+//Hola Mundo en Java
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        int miVariable = 5;
+        String miVar = "HELLO";
+        System.out.println(miVariable);
+        miVariable = 10;
+        System.out.println(miVariable);
+        System.out.println(miVar);
+    }
+}
---
+++ Python/Leccion 1/miVariable.py
@@ -0,0 +1,6 @@
+miVariable = 3
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
